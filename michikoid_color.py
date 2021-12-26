b533d35013c7477038ce752e52471fd92b453923
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2021-12-26 15:12:43 -0300

    Update michikoid_color.py

--- michikoid_color.py
+++ michikoid_color.py
@@ -141,7 +141,7 @@ def build_request():
     # Get User Agent:
     user_agent = input(Fore.RESET + "User-Agent: " + Fore.CYAN)
     if user_agent == "":
-        user_agent = "Michikoid-Repeater"
+        user_agent = "Mozilla/5.0, Gecko/20100101 Michikoid-Repeater/1.1"
     # Get Referer link:
     referer = input(Fore.RESET + "Referer link: " + Fore.CYAN).lower()
     if referer == "":
